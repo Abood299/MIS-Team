a7cefb6aacf600ff594752ad8589f2cbbf5762fb
Abood299 <abood.qudah26@gmail.com>
2025-04-09 18:45:56 +0300

    Updated BusinessHub folder
    Fixed deps 7 pages made trees 2 per row, and removed extra titles and spaces , fixed grey menu and x button , fixed redunduncy on certain pages < Enhanced Main page and more responsive layout and unifined font across pages and ton of minor edits across all pages

---
+++ businesshub/connect.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$conn = mysqli_connect('localhost', 'abdullah', 'Abood_123', 'booksexchange');
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
