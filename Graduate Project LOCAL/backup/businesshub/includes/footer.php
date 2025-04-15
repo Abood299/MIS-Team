2d18d18afe911ee39ba460c42a3c520b423762ee
Abood299 <abood.qudah26@gmail.com>
2025-04-15 22:50:23 +0300

    added local folder

---
+++ Graduate Project LOCAL/backup/businesshub/includes/footer.php
@@ -0,0 +1,59 @@
+<!--  FOOTER 🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥--> 
+<footer class="footer">
+  <div class="footer-container">
+    <div class="footer-left">
+      <h2>
+        <a href="MainPagefull.php" style="text-decoration: none;">
+          <span style="color: #EC522D; font-weight: bold; text-shadow: 0 0 5px #EC522D, 0 0 20px #EC522D;">Business</span>
+          <span style="color: #5E2950; font-weight: bold; text-shadow: 0 0 10px #5E2950, 0 0 20px #5E2950;">Hub</span>
+        </a>
+      </h2>
+      <p class="slogan">تمكين الطلاب وربط المعرفة وبناء المستقبل</p>
+    <p class="copyright">جميع الحقوق محفوظة © 2025 <strong>Business Hub</strong></p>
+
+    </div>
+
+
+    <div class="footer-middle">
+      <a href="MIS.php">نظم المعلومات الإدارية</a>
+      <a href="BUS.php">إدارة الأعمال</a>
+      <a href="ECO.php">اقتصاد الأعمال</a>
+      <a href="PBUS.php">الإدارة العامة</a>
+      <a href="FNC.php">التمويل</a>
+      <a href="MKT.php">التسويق</a>
+      <a href="ACC.php">المحاسبة</a>
+    </div>
+
+
+
+    <div class="footer-right">
+      <a href="https://regapp.ju.edu.jo/regapp/">موقع التسجيل</a>
+      <a href="https://elearning.ju.edu.jo/login/index.php">eLearning</a>
+      <a href="https://juexams.com/moodle/login/index.php">Ju-Exam</a>
+      <a href="https://library.ju.edu.jo/Elibrary/">E-Library</a>
+      <a href="https://eservices.ju.edu.jo/ClinicApp/Home.aspx">عيادة الجامعة</a>
+      <a href="#">من نحن</a>
+    </div>
+
+  </div>
+
+</footer>
+
+ 
+      <!--  FOOTER 🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
